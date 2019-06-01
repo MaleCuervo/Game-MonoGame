f2d8d76e54e60a16dae8417af5333cdef1dcc548
malecuervo <alejaxz23@gmail.com>
2019-06-01 18:58:27 -0300

    Add project

---
+++ Final-Programacion-Aplicada/Game6/Models/Input.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game6.Models
+{
+    public class Input
+    {
+        public Keys Up { get; set; }
+
+        public Keys Down { get; set; }
+
+        public Keys Left { get; set; }
+
+        public Keys Right { get; set; }
+
+        public Keys Shoot { get; set; }
+    }
+}
+
